ab39b6f70b6326619bc5dcc569bf9ee37d9da578
nargis1776 <122875450+nargis1776@users.noreply.github.com>
2023-08-14 20:08:25 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import Task4 from "./Task4/Main"
+import Task3 from "./Task3/Main"
+import Task2b from "./Task2b/Main"
+import "./App.css"
+function App() {
+  return (
+    <>
+   {/* <Task4/> */}
+   {/* <Task3/>
+    */}
+    <Task2b/>
+    </>
+  );
+}
+
+export default App;
